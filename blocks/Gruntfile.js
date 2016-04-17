167d0911d696bfc54187005ecbc00c0acf55bcc2
simonpatrat <simonpatrat@users.noreply.github.com>
2016-04-17 18:48:53 +0200

    Update Grunt config

--- blocks/Gruntfile.js
+++ blocks/Gruntfile.js
@@ -1,32 +1,34 @@
 module.exports = function(grunt) {
 
+	require('load-grunt-tasks')(grunt);
+
 	// Project configuration. 
 	grunt.initConfig({
-	  concat: {
-	    options: {
-	      separator: ';',
-	    },
-	    dist: {
-	      src: ['js/customizer.js', 'js/navigation.js', 'js/skip-link-focus-fix.js'],
-	      dest: 'dist/site.min.js',
-	    },
+
+	  jshint: {
+	    all: ['js/*.js']
 	  },
+
 
 	  uglify: {
 	    dist: {
 	      files: {
-	        'dist/site.min.js': ['dist/site.min.js']
+	        'dist/site.min.js': ['js/customizer.js', 'js/navigation.js', 'js/skip-link-focus-fix.js']
 	      }
 	    }
+	  },
+	
+	  watch: {
+	  	dist: {
+	  		files: ['js/*.js', '!**/min.*'],
+	  		tasks: ['default'],
+	  		options: {spawn: false }
+	  	}
 	  }
+
 
 	});
 
+	grunt.registerTask('default', ['jshint', 'uglify']);
 
-
-	grunt.loadNpmTasks('grunt-contrib-concat');
-
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-
-	grunt.registerTask('default', ['concat', 'uglify']);
 }
